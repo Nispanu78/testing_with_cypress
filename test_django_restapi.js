2598b2cedb195a74fa258f13bf9732b724bf9b47
Nicola <nicspanu@gmail.com>
2021-05-24 11:16:14 +0200

    test django restapi

--- test_django_restapi.js
+++ test_django_restapi.js
@@ -8,26 +8,26 @@ describe('The Home Page', () => {
     // Find "form-group"
     cy.get('.form-group')
     // Find the input tag first and type a message
-    cy.get('input[type=text]').type('Cypress is fantastic!')
-    // Find the textarea second and type a message
-    cy.get('textarea[name=body]').type('Happy to use Cypress')
+    cy.get('input[type=text]').type('I love Cypress!')
+    // Find the textarea tag second and type a message
+    cy.get('textarea[name=body]').type('I really love it!')
     // Post the message by clicking on the POST button
-    cy.contains('POST').click()
+    cy.get('button').contains('POST').click()
     // Visit the webpage where the message is
-    cy.visit('http://127.0.0.1:8000/api/v1/10/')
-    // Let us clear all field
+    cy.visit('http://127.0.0.1:8000/api/v1/14/')
+    // Clear input field
     cy.get('input[type=text]').clear()
-    // Find the textarea second and type a message
+    // Clear textarea field
     cy.get('textarea[name=body]').clear()
-    // Let us update the fields and click PUT
-    cy.get('input[type=text]').type('This is not a test but a real post')
-    // Find the textarea second and type a message
-    cy.get('textarea[name=body]').type('Indeed!')
-    cy.contains('PUT').click()
-    cy.visit('http://127.0.0.1:8000/api/v1/10/')
-    // let us delete the POST
-    cy.contains('DELETE').click()
-
-
+    // Update the input field
+    cy.get('input[type=text]').type('But it is a sunny day')
+    // Update the textarea field
+    cy.get('textarea[name=body]').type('And I would like to spend my day outside')
+    // Update the post by clicking on PUT
+    cy.get('button').contains('PUT').click()
+    // Visit updated page
+    cy.visit('http://127.0.0.1:8000/api/v1/14/')
+    // DELETE the post
+    cy.get('button').contains('DELETE').click()
   })
 })
